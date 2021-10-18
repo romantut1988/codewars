1272628ba3a7b7ffc5edef8d428d06d8846dcc1d
romantut1988 <romantut1988@mail.ru>
2021-10-18 14:40:44 +0300

    Convert number to reversed array of digits

---
+++ script-task3.js
@@ -0,0 +1,10 @@
+function digitize(n) {
+    var initialArray = (""+n).split('');
+    var reversedArray = [];
+    for (var i = initialArray.length - 1; i >= 0; i--) {
+      reversedArray[i] = parseInt(initialArray.shift(),10);
+    }
+    return reversedArray;
+  }
+  
+  console.log(digitize(348597));
