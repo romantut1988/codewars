0591f4e16ef6dca8217539f2771fb5cfffda0042
romantut1988 <romantut1988@mail.ru>
2021-10-19 23:55:10 +0300

    Double char

---
+++ script-task.17.js
@@ -0,0 +1,7 @@
+function doubleChar(str) {
+    return [...str].map(s => s.repeat(2)).join('');
+}
+
+console.log(doubleChar("String"));  
+console.log(doubleChar("Helloddd World"));
+console.log(doubleChar("1234!_ "));
