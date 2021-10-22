cf00548f4a57fe890e56d9c207553e9b81ad0f9a
romantut1988 <romantut1988@mail.ru>
2021-10-23 00:40:55 +0300

    Return two highest values in List

--- script-task61.js
+++ script-task61.js
@@ -1,10 +1,11 @@
 function twoHighest(arr) {
     return arr
-      .filter((v, i, a) => a.indexOf(v) === i)  // Unique array
-      .sort((a, b) => b - a)  // Sort by DESC
-      .slice(0, 2);  // Get first 2 element of sorted array
+        .filter((v, i, a) => a.indexOf(v) === i)  
+        .sort((a, b) => b - a)  
+        .slice(0, 2);  
     };
-
+    
     const demoArray = [15, 20, 20, 17];
     const result = twoHighest(demoArray);
+
     console.log(result);
